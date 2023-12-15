027cfe83df65a8a6bf2d841b9a6e5b426073ed3c
biruytskovsky <david.biruykov.02@mail.ru>
2023-12-15 22:34:35 +0200

    Добавьте файлы проекта.

---
+++ zadacha 9/zadacha 9.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+// Функция для проверки, является ли число простым
+bool isPrime(int num) {
+    if (num < 2) {
+        return false;
+    }
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    // Исходная матрица
+    std::vector<std::vector<int>> matrix = { {2, 3, 5},
+                                           {4, 7, 11},
+                                           {1, 8, 15} };
+
+    // Поиск максимального элемента в матрице
+    int maxElement = matrix[0][0];
+    for (const auto& row : matrix) {
+        for (int num : row) {
+            if (num > maxElement) {
+                maxElement = num;
+            }
+        }
+    }
+
+    // Подсчет простых чисел в каждой строке
+    std::vector<std::pair<int, int>> primeCount;  // Вектор для хранения пар (номер строки, количество простых чисел)
+    for (int i = 0; i < matrix.size(); ++i) {
+        int count = 0;
+        for (int j = 0; j < matrix[i].size(); ++j) {
+            if (isPrime(matrix[i][j])) {
+                count++;
+            }
+        }
+        primeCount.emplace_back(i, count);
+    }
+
+    // Упорядочивание строк по невозрастанию количества простых чисел
+    std::sort(primeCount.begin(), primeCount.end(), [](const std::pair<int, int>& a, const std::pair<int, int>& b) {
+        return a.second >= b.second;
+        });
+
+    // Новая упорядоченная матрица
+    std::vector<std::vector<int>> sortedMatrix(matrix.size(), std::vector<int>(matrix[0].size(), -1));
+    for (int i = 0; i < primeCount.size(); ++i) {
+        sortedMatrix[i] = matrix[primeCount[i].first];
+    }
+
+    // Вывод результата
+    for (const auto& row : sortedMatrix) {
+        for (int num : row) {
+            std::cout << num << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
